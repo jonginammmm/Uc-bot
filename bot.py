0917db83135415b5cde52f5f5155d09d434254b1
jonginammmm <ortiqboyevoybek641@gmail.com>
2026-03-31 22:08:54 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,12 @@
+import telebot
+import os
+
+TOKEN = os.getenv "8716951130:AAEiikCG797kjvknQU3YuAFe-cyp5FJ3RFs"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Bot ishlayapti 🚀")
+
+bot.infinity_polling()
