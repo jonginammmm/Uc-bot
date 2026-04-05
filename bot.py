7d9c9fb5663dbc0071bdc160ac6c1c1989542c3a
jonginammmm <ortiqboyevoybek641@gmail.com>
2026-04-05 07:52:16 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,32 @@
-import telebot
 import os
+import telebot
 from flask import Flask
 import threading
 
+# TOKEN olish
 TOKEN = os.getenv("8716951130:AAEiikCG797kjvknQU3YuAFe-cyp5FJ3RFs")
+
+if not TOKEN:
+    raise ValueError("❌ TOKEN topilmadi! Render ENV ga qo‘sh!")
+
 bot = telebot.TeleBot(TOKEN)
 
+# /start komandasi
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Salom 👋 CoMETA Botingiz ishlayapti muammolar❌!")
+    bot.reply_to(message, "Salom 👋 CoMETA siz yaratgan UC Bot ishlayapti!")
 
-# KEEP ALIVE
-app = Flask('')
+# oddiy javob (test uchun)
+@bot.message_handler(func=lambda message: True)
+def echo(message):
+    bot.reply_to(message, message.text)
+
+# Flask (Render o‘chib qolmasligi uchun)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Alive"
+    return "Bot ishlayapti!"
 
 def run():
     app.run(host='0.0.0.0', port=10000)
@@ -24,6 +35,8 @@ def keep_alive():
     t = threading.Thread(target=run)
     t.start()
 
-keep_alive()
-
-bot.infinity_polling()
+# ishga tushirish
+if __name__ == "__main__":
+    keep_alive()
+    print("✅CoMETA UC Botiz ishga tushdi💋...")
+    bot.infinity_polling()
